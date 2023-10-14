597dc4c4b832cb995f476b244791f8bf462a8ace
danrev21 <danrev21@github.com>
2023-10-14 20:12:04 +0300

    changed fd2.sh file

--- fd2.sh
+++ fd2.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-echo "This is a standard output that will be silenced" 
-echo "This is an error message that will be silenced" >&2
-exit 0
+
+echo "This is a standard output that will be silenced" > /dev/null
+echo "This is an error message that will be silenced" >&2 > /dev/null
