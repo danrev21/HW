dda14fc958a0c1a53c2e0bfa930f5ad6c19d73ec
danrev21 <dan.rev2021@gmail.com>
2023-10-21 20:12:39 +0000

    triangle.sh v1.0 prod

--- bash_lesson_02/triangle.sh
+++ bash_lesson_02/triangle.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-echo  "Value of triangle sides: $1 $2 $3"
-
+# Matching arguments to regex positive integer between 1 and 100
 if [[ ! $1 =~ ^[1-9][0-9]?$|^100$ ]] || [[ ! $2 =~ ^[1-9][0-9]?$|^100$ ]] || [[ ! $3 =~ ^[1-9][0-9]?$|^100$ ]];
-   then echo "Wrong"; exit;
+   then echo "Input is wrong. Restart script."; exit;
 fi
-
+# Checking triangle type
 if [[ $1 -ne $2 ]] && [[ $1 -ne $3 ]] && [[ $2 -ne $3 ]];
    then echo "SCALENE";
 elif [[ $1 -eq $2 ]] && [[ $1 -eq $3 ]] && [[ $2 -eq $3 ]];
