066de9ab45ab7f1b9fc99c3f5e30484b747acab1
danrev21 <dan.rev2021@gmail.com>
2023-10-21 20:01:33 +0000

    triangle.sh v0.5

--- bash_lesson_02/triangle.sh
+++ bash_lesson_02/triangle.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-echo  "Value : $1"
+echo  "Value of triangle sides: $1 $2 $3"
 
-if [[ ! $1 =~ ^[1-9][0-9]?$|^100$ ]] ;
-   then echo "Wrong";
+if [[ ! $1 =~ ^[1-9][0-9]?$|^100$ ]] || [[ ! $2 =~ ^[1-9][0-9]?$|^100$ ]] || [[ ! $3 =~ ^[1-9][0-9]?$|^100$ ]];
+   then echo "Wrong"; exit;
 fi
 
-
+if [[ $1 -ne $2 ]] && [[ $1 -ne $3 ]] && [[ $2 -ne $3 ]];
+   then echo "SCALENE";
+elif [[ $1 -eq $2 ]] && [[ $1 -eq $3 ]] && [[ $2 -eq $3 ]];
+   then echo "EQUILATERAL";
+   else echo "ISOSCELES";
+fi
